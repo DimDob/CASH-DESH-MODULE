dd0f5125d0b56961c51e17a7531ed03c4828b833
DimDob <dimitardobrev.business@gmail.com>
2024-01-07 21:32:27 +1300

    Fix name of app

--- demo/src/main/java/com/example/CashDeskModule/CashDeshModuleApplication.java
+++ demo/src/main/java/com/example/CashDeskModule/CashDeshModuleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CashDeskModuleApplication {
+public class CashDeshModuleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CashDeskModuleApplication.class, args);
+		SpringApplication.run(CashDeshModuleApplication.class, args);
 	}
 
 }
